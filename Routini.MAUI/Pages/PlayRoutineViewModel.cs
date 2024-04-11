9f607ce311e43c7f09d771ede1c2ca03db6ab720
SingletonSean <singletonsean4@gmail.com>
2024-04-11 15:35:18 -0400

    Add basic timer behavior

--- Routini.MAUI/Pages/PlayRoutineViewModel.cs
+++ Routini.MAUI/Pages/PlayRoutineViewModel.cs
@@ -1,6 +1,8 @@
 ﻿using CommunityToolkit.Mvvm.ComponentModel;
+using CommunityToolkit.Mvvm.Input;
 using Routini.MAUI.Entities.Routines;
 using Routini.MAUI.Features.ListRoutines;
+using System.Timers;
 
 namespace Routini.MAUI.Pages
 {
@@ -9,7 +11,10 @@ namespace Routini.MAUI.Pages
         private readonly GetRoutineByIdQuery _query;
 
         [ObservableProperty]
-        private string? _name;
+        [NotifyPropertyChangedFor(nameof(Name))]
+        private Routine? _routine;
+
+        public string Name => Routine?.Name ?? string.Empty;
 
         [ObservableProperty]
         private string? _errorMessage;
@@ -31,9 +36,7 @@ namespace Routini.MAUI.Pages
             {
                 Guid id = Guid.Parse(queryParameters["Id"]?.ToString() ?? "");
 
-                Routine routine = await _query.Execute(id);
-
-                Name = routine.Name;
+                Routine = await _query.Execute(id);
             }
             catch (Exception)
             {
@@ -42,6 +45,86 @@ namespace Routini.MAUI.Pages
             finally
             {
                 Loading = false;
+            }
+        }
+
+        private System.Timers.Timer _timer = new System.Timers.Timer();
+        private int _currentStepIndex = 0;
+        private DateTimeOffset _currentStepStartTime;
+
+        [ObservableProperty]
+        [NotifyPropertyChangedFor(nameof(CurrentStepName))]
+        private RoutineStep? _currentStep;
+
+        public string CurrentStepName => CurrentStep?.Name ?? string.Empty;
+
+        public double CurrentStepSecondsRemaining
+        {
+            get
+            {
+                if (CurrentStep == null)
+                {
+                    return 0;
+                }
+
+                return Math.Ceiling(
+                    CurrentStep.Duration.TotalSeconds - 
+                    DateTimeOffset.Now.Subtract(_currentStepStartTime).TotalSeconds);
+            }
+        }
+
+        [RelayCommand]
+        private void StartRoutine()
+        {
+            if (Routine == null)
+            {
+                return;
+            }
+
+            _timer.Interval = 250;
+            _timer.Elapsed += OnTimerElapsed;
+            _timer.Start();
+
+            _currentStepIndex = 0;
+            _currentStepStartTime = DateTimeOffset.Now;
+            CurrentStep = Routine.Steps.ElementAt(_currentStepIndex);
+        }
+
+        [RelayCommand]
+        private void PauseRoutine()
+        {
+            _timer.Stop();
+        }
+
+        [RelayCommand]
+        private void ResumeRoutine()
+        {
+            _timer.Start();
+        }
+
+        [RelayCommand]
+        private void CancelRoutine()
+        {
+            _timer.Stop();
+            _timer.Elapsed -= OnTimerElapsed;
+            _currentStepIndex = 0;
+            CurrentStep = null;
+        }
+
+        private void OnTimerElapsed(object? sender, ElapsedEventArgs e)
+        {
+            if (Routine == null)
+            {
+                return;
+            }
+
+            OnPropertyChanged(nameof(CurrentStepSecondsRemaining));
+
+            if (CurrentStepSecondsRemaining <= 0)
+            {
+                _currentStepIndex += 1;
+                _currentStepStartTime = DateTimeOffset.Now;
+                CurrentStep = Routine.Steps.ElementAt(_currentStepIndex);
             }
         }
     }
