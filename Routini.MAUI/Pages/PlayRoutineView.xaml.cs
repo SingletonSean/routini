b2da527f459103ae65a7d70bbb33fab66f9c1e9c
SingletonSean <singletonsean4@gmail.com>
2024-04-11 19:48:17 -0400

    End routine on navigation

--- Routini.MAUI/Pages/PlayRoutineView.xaml.cs
+++ Routini.MAUI/Pages/PlayRoutineView.xaml.cs
@@ -1,11 +1,29 @@
+using System.Windows.Input;
+
 namespace Routini.MAUI.Pages;
 
 public partial class PlayRoutineView : ContentPage
 {
-	public PlayRoutineView(PlayRoutineViewModel viewModel)
+    public static readonly BindableProperty OnDisappearingCommandProperty =
+    BindableProperty.Create(nameof(OnDisappearingCommand), typeof(ICommand), typeof(ListRoutinesView), null);
+
+    public ICommand OnDisappearingCommand
+    {
+        get => (ICommand)GetValue(OnDisappearingCommandProperty);
+        set => SetValue(OnDisappearingCommandProperty, value);
+    }
+
+    public PlayRoutineView(PlayRoutineViewModel viewModel)
 	{
 		InitializeComponent();
 
 		BindingContext = viewModel;
 	}
+
+    protected override void OnDisappearing()
+    {
+        OnDisappearingCommand?.Execute(null);
+
+        base.OnDisappearing();
+    }
 }
