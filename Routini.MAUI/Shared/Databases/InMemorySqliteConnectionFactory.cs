f323f08ce81d8b56c645e7772b83f76126f739b6
SingletonSean <singletonsean4@gmail.com>
2024-04-14 21:42:17 -0400

    Scope in memory database per test

--- Routini.MAUI/Shared/Databases/InMemorySqliteConnectionFactory.cs
+++ Routini.MAUI/Shared/Databases/InMemorySqliteConnectionFactory.cs
@@ -4,9 +4,16 @@ namespace Routini.MAUI.Shared.Databases
 {
     public class InMemorySqliteConnectionFactory : ISqliteConnectionFactory
     {
+        private readonly Guid _id;
+
+        public InMemorySqliteConnectionFactory()
+        {
+            _id = Guid.NewGuid();
+        }
+
         public ISQLiteAsyncConnection Create()
         {
-            return new SQLiteAsyncConnection("Data Source=InMemorySample;Mode=Memory;Cache=Shared");
+            return new SQLiteAsyncConnection($"Data Source={_id};Mode=Memory;Cache=Shared");
         }
     }
 }
