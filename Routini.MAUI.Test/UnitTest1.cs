5b873678d3196d4696b14d447a9a1df389a1e58b
SingletonSean <singletonsean4@gmail.com>
2024-04-14 21:08:52 -0400

    Setup integration test project

---
+++ Routini.MAUI.Test/UnitTest1.cs
@@ -0,0 +1,16 @@
+namespace Routini.MAUI.Test
+{
+    public class Tests
+    {
+        [SetUp]
+        public void Setup()
+        {
+        }
+
+        [Test]
+        public void Test1()
+        {
+            Assert.Pass();
+        }
+    }
+}
